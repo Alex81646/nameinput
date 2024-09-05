682b39b29f56488cdcc7d23093daf761009df66a
isai <gutieisa001@warren.k12.in.us>
2024-09-05 13:48:44 -0400

    added initial

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("isai gutierrez : ")
+#INSTRUCTION : write an input
+# statement that asks for
+# number of droid and store as droids
+INSTRUCTION:  write an input
+# statement that asks for
+# number of wookies and store as wookies
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
